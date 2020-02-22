9c8ea2e9125edf159d19cef2d999f0232aefe444
Jake Garcia <bballwiz4@gmail.com>
2020-02-22 16:45:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,9 @@ def line(line_arr)
   if line_arr.size == 0
     puts "The line is currently empty."
   elsif line_arr.size > 0
-    line_string = "The line is currently: "
+    line_string = "The line is currently:"
     line_arr.each_with_index do |name, i|
-      line_string << "#{i + 1}. #{name} "
+      line_string << " #{i + 1}. #{name}"
     end
     puts line_string
   end
