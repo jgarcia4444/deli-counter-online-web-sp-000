f379a0ee80e1384f776e3bd1519049cc6a463a9c
Jake Garcia <bballwiz4@gmail.com>
2020-02-22 16:44:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,5 +2,11 @@
 def line(line_arr)
   if line_arr.size == 0
     puts "The line is currently empty."
+  elsif line_arr.size > 0
+    line_string = "The line is currently: "
+    line_arr.each_with_index do |name, i|
+      line_string << "#{i + 1}. #{name} "
+    end
+    puts line_string
   end
 end
